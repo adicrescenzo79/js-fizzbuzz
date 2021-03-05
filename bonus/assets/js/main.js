28571de96938a5760fa7f065cfd3b600839ba936
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-05 14:37:51 +0100

    bonus fatto

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,19 +1,19 @@
-var numeri = [];
+numero = prompt("dammi un numero tra 1  e 100:");
 
-for (var i = 1; i <= 100; i++) {
-  numeri.push(i);
+if (!(numero%3) && !(numero%5)) {
+  numero = "FizzBuzz";
+  document.body.style.backgroundImage = "url('./assets/img/oro.jpg')";
+  document.body.style.backgroundSize = "cover";
+}else if (!(numero%3)) {
+  numero = "Fizz";
+  document.body.style.backgroundImage = "url('./assets/img/bronzo.jpg')";
+  document.body.style.backgroundSize = "cover";
+} else if (!(numero%5)) {
+  numero = "Buzz";
+  document.body.style.backgroundImage = "url('./assets/img/argento.jpg')";
+  document.body.style.backgroundSize = "cover";
 }
 
-console.log(numeri);
+console.log(numero);
 
-for (var i = 0; i < numeri.length; i++) {
-  if (!(numeri[i]%3) && !(numeri[i]%5)) {
-    numeri[i] = "FizzBuzz";
-  }else if (!(numeri[i]%3)) {
-    numeri[i] = "Fizz";
-  } else if (!(numeri[i]%5)) {
-    numeri[i] = "Buzz";
-  }
-}
-
-console.log(numeri);
+document.getElementById("nr").innerHTML = numero;
